531582e6a8ead2448a8d96d85d42891f96423ff6
Leo Felippe <leofelippedias@hotmail.com.br>
2016-08-08 17:06:15 -0300

    edicoes menores

--- iniciar.js
+++ iniciar.js
@@ -13,13 +13,11 @@
 var sistemaDeArquivo = require('fs');
 var pasta = require('path');
 var pastaDeConfiguracaoPadrao = pasta.join(__dirname, '/configuracao/configuracao.js');
-var http = require('http');
-var https = require('https');
-var morgan = require('morgan');  // Oferece registro para as requisições http do express
 var express = require('express');
-var registrador = require('./fonte/nucleo/registrador')('iniciar');  // Carregamos o nosso registrador
 var configurado = require('configurado');
+var registrador = require('./fonte/nucleo/registrador')('iniciar');  // Carregamos o nosso registrador
 var registradorDoConfigurado = require('./fonte/nucleo/registrador')('configurado');
+var Expressando = require('expressando');
 
 configurado.iniciar(pastaDeConfiguracaoPadrao, registradorDoConfigurado, function(configuracao) {
   
@@ -29,93 +27,38 @@ configurado.iniciar(pastaDeConfiguracaoPadrao, registradorDoConfigurado, functio
   // Nossa configuração do servidor Express.
   var confDoServidor = configuracao.servidor;
   
-  // Nossa configuração do CORS.
-  var confDoCors = configuracao.servidor.cors;
-  
-  // Aqui temos as origens permitidas no nosso serviço CORS. Lembre-se que
-  // iremos oferecer dois tipos de conexões (http e https).
-  var listaDasOrigensPermitidas = confDoCors.origem;
-
-  /* Iremos separar as preocupações do nosso projeto, para isso nós iremos
-   * oferecer os serviços deste servidor para a parte da visão. Assim iremos
-   * oferecer aceitação de conexões e requisições dos dominios de origem
-   * permitidos utilizando o módulo CORS.
-   * @Veja https://www.npmjs.com/package/cors
-   */
-  var cors = require('cors');
-  aplicativo.use(cors({
-    origin: function(origem, cd) {  // Origem aceita por este servidor express.
-      var seOrigemPermitida = listaDasOrigensPermitidas.indexOf(origem) !== -1;
-      cd(null, seOrigemPermitida);
-    }  
-  , methods:  confDoCors.metodos // Métodos aceitos.
-  , allowedHeaders: confDoCors.cabecalhosAceitos
-  , exposedHeaders: confDoCors.cabecalhosExpostos  
-  , credentials: confDoCors.seUsarCredenciais
-  }));
-  
   // Aqui temos a nossa chave e certificado para nosso servidor https.
   var chavePrivada  = sistemaDeArquivo.readFileSync('./certificados/' + confDoServidor.certificados.chavePrivada, 'utf8');
   var certificado = sistemaDeArquivo.readFileSync('./certificados/' + confDoServidor.certificados.certificado, 'utf8');
-  var credenciais = {key: chavePrivada, cert: certificado};
+  var credenciais = { key: chavePrivada, cert: certificado };
   
-  /* Utilizamos o bodyParser para receber requisições POST ou PUT. Lembre-se de
-   * manter o limit do body em 200kb para nos precaver dos ataques de negação de
-   * serviço.
-   */
-  var bodyParser = require('body-parser');
-  aplicativo.use(bodyParser.json({limit: confDoServidor.limite}));
-  aplicativo.use(bodyParser.urlencoded({limit: confDoServidor.limite, extended: false}));
+  var listaDeRotas = [
+    // Iremos servir os arquivos do diretorio "/testes"
+    { "caminho": express.static(pasta.join(__dirname, 'testes')), "rota": '/' }
   
-  // Porta ao qual iremos receber requisições http.  
-  aplicativo.set('porta', process.env.PORT || confDoServidor.porta);
+    // Iremos servir os arquivos do diretorio "testes/incluir/js/bibliotecas"
+  , { "caminho": express.static(pasta.join(__dirname, 'testes/incluir/js/bibliotecas')), "rota": '/bibliotecas' }
   
-  // Porta ao qual iremos receber requisições https.  
-  aplicativo.set('portaSSL', process.env.SSLPORT || confDoServidor.portaSSL);
-  
-  // Adicionamos isso para realizar o registro de requisições.
-  aplicativo.use(morgan(confDoServidor.registro || 'combined')); 
-  
-  // Aqui nós iremos obrigado que as conexões não seguras sejam redirecionadas.
-  // Para mais informações @veja http://stackoverflow.com/a/10715802
-  aplicativo.use(function(req, res, proximo) {
-    // Se a requisição não for segura.
-    if(!req.secure && confDoServidor.exigirConSegura) {
-      // Aqui faremos redirecionar para uma conexão segura.
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    proximo();
-  });
-  
-  // Iremos servir os arquivos do diretorio "/testes"
-  aplicativo.use('/', express.static(pasta.join(__dirname, 'testes')));  
-  
-  // Iremos servir os arquivos do diretorio "testes/incluir/js/bibliotecas"
-  aplicativo.use('/bibliotecas', express.static(pasta.join(__dirname, 'testes/incluir/js/bibliotecas'))); 
+    // Iremos servir os arquivos do diretorio "testes/incluir/estilos"
+  , { "caminho": express.static(pasta.join(__dirname, 'testes/incluir/estilos')), "rota": '/estilos' }
+  ];
 
-  // Iremos servir os arquivos do diretorio "testes/incluir/estilos"
-  aplicativo.use('/estilos', express.static(pasta.join(__dirname, 'testes/incluir/estilos'))); 
+  var expressando = new Expressando({
+    "configuracao": configuracao,
+    "aplicativo": aplicativo,
+    "credenciais": credenciais,
+    "lista": listaDeRotas
+  }, function(objExpressando){ objExpressando.carregar(); });
   
   // Chamamos o arquivo principal, ele vai carregar os outros arquivos
   // principais do servidor.
   var principal = require('./fonte/iniciador/principal');
   
   principal.prosseguir(configuracao, aplicativo, function() {
-    
     registrador.debug('Carregando o servidor HTTP e HTTPS.');
-    
-    // Inicia o servidor HTTP e começa a esperar por conexões.
-    aplicativo.servidor = http.createServer(aplicativo);
-    aplicativo.servidor.listen(aplicativo.get('porta'), function () {
-      registrador.debug("Servidor HTTP express carregado e escutando na porta " + aplicativo.get('porta'));
+    expressando.escutar(function(){
+      // pronto
     });
-    
-    // Inicia o servidor HTTPS e começa a esperar por conexões.
-    aplicativo.servidorSSL = https.createServer(credenciais, aplicativo);
-    aplicativo.servidorSSL.listen(aplicativo.get('portaSSL'), function () {
-      registrador.debug("Servidor HTTPS express carregado e escutando na porta " + aplicativo.get('portaSSL'));
-    });
-    
   });
   
 });
